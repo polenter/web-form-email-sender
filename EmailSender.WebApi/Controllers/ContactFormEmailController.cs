02e74e91d7a7cef5603e74012c608e75d7ce16c1
polenter <29297999+polenter@users.noreply.github.com>
2022-02-18 11:32:13 +0100

    Logging implemented in Web API.

--- EmailSender.WebApi/Controllers/ContactFormEmailController.cs
+++ EmailSender.WebApi/Controllers/ContactFormEmailController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using EmailSender.WebApi.Services;
 using EmailSender.WebApi.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace EmailSender.WebApi.Controllers
 {
@@ -11,13 +12,15 @@ namespace EmailSender.WebApi.Controllers
     public class ContactFormEmailController : ControllerBase
     {
         private readonly IEmailSender _emailSender;
+        private readonly ILogger<ControllerBase> _logger;
         private readonly IFormClientRepository _formClientRepository;
 
         public ContactFormEmailController(IFormClientRepository formClientRepository,
-            IEmailSender emailSender)
+            IEmailSender emailSender, ILogger<ContactFormEmailController> logger)
         {
             _formClientRepository = formClientRepository;
             _emailSender = emailSender;
+            _logger = logger;
         }
 
         [HttpPost]
@@ -25,18 +28,29 @@ namespace EmailSender.WebApi.Controllers
         {
             if (!HoneyPotValidate(viewModel))
             {
-                await Task.Delay(1700);
-                return Ok();
+                _logger.LogDebug(FormatLogMessage(viewModel, "Honey pot!"));
+                return await FakeOk();
             }
 
             var clientMetadata = await _formClientRepository.GetClientAsync(viewModel.ClientId);
-            if (clientMetadata == null) return KeyNotFound();
+            if (clientMetadata == null)
+            {
+                _logger.LogDebug(FormatLogMessage(viewModel, "Client not found."));
+                return KeyNotFound();
+            }
 
             var subject = string.Format(Captions.EmailSubject, clientMetadata.Website);
             await _emailSender.SendEmailAsync(viewModel.SenderEmail, clientMetadata.TargetEmail, subject,
                 viewModel.Text);
 
+            _logger.LogInformation(FormatLogMessage(viewModel, "Message sent."));
             return Ok();
+        }
+
+        private string FormatLogMessage(SendContactFormMessageViewModel viewModel, string? message = "")
+        {
+            var model = JsonConvert.SerializeObject(viewModel);
+            return $"{message} {model}.".Trim();
         }
 
         private bool HoneyPotValidate(SendContactFormMessageViewModel viewModel)
@@ -48,6 +62,12 @@ namespace EmailSender.WebApi.Controllers
         {
             ModelState.AddModelError(nameof(SendContactFormMessageViewModel.ClientId), "Unknown client.");
             return BadRequest();
+        }
+
+        private async Task<IActionResult> FakeOk()
+        {
+            await Task.Delay(1700);
+            return Ok();
         }
     }
 }
